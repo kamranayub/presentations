52367012506a02e790a5099ed1fb20d4ded174c0
Kamran Ayub <kamran.ayub@gmail.com>
2016-04-11 18:20:50 -0500

    Add example of type alias and union types for sorting

---
+++ typescript-in-action/demo/DemoApp/Models/SortBy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp.Models
+{
+    public enum SortBy
+    {
+        Name,
+        Author,
+        ReleaseDate
+    }
+}
