f9a55ac3a997fa07304797d5e0c0173d99cdc48a
Kamran Ayub <kamran.ayub@gmail.com>
2018-03-27 23:16:01 -0500

    Couple changes

--- demystifying-typescript/example/errors/simple.js
+++
@@ -1,8 +0,0 @@
-function sortByName(a) {
-    var result = a.slice(0);
-    result.sort(function (x, y) {
-        return x.name.localCompare(y.name);
-    });
-    return result;
-}
-//# sourceMappingURL=simple.js.map
